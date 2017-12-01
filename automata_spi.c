b30fe13f986d3cc511a63280bbb11a355286ddd9
kostarasae <32982168+kostarasae@users.noreply.github.com>
2017-12-01 17:10:50 +0000

    Update automata_spi.c

--- automata_spi.c
+++ automata_spi.c
@@ -1,3 +1,11 @@
+/*
+ * automata_spi.c
+ *
+ *  Created on: 28 Nov 2017
+ *      Author: Kostas Stamatakis
+ */
+
+
 #include "automata_spi.h"
 
 
@@ -6,62 +14,61 @@ SPI_RETURN_T SPI_configureTransfer(const SPI_CONFIG_T *config)
 {
 	if (config == NULL)
 	{
-		return _SPI_RETURN.SPI_RETURN_nullptr;
+		return SPI_RETURN_T::SPI_RETURN_nullptr;
 	}
 	else
 	{
-		if (config.cs_pol < SPI_POLARITY_T.SPI_ACTIVE_LOW ||
-			config.cs_pol > SPI_POLARITY_T.SPI_ACTIVE_HIGH)
+		if (config.cs_pol < SPI_POLARITY_T::SPI_ACTIVE_LOW ||
+			config.cs_pol > SPI_POLARITY_T::SPI_ACTIVE_HIGH)
 		{
-			return _SPI_RETURN.SPI_RETURN_invalid_arg;
+			return SPI_RETURN_T::SPI_RETURN_invalid_arg;
 		}
-		
-		if (config.cpol < SPI_CPOL_T.SPI_IDLE_LOW ||
-			config.cpol > SPI_CPOL_T.SPI_IDLE_HIGH)
+
+		if (config.cpol < SPI_CPOL_T::SPI_IDLE_LOW ||
+			config.cpol > SPI_CPOL_T::SPI_IDLE_HIGH)
 		{
-			return _SPI_RETURN.SPI_RETURN_invalid_arg;
-		}		
-				
-		if (config.cpha < SPI_CHPA_T.SPI_EDGE_RISING ||
-			config.cpha > SPI_CHPA_T.SPI_EDGE_FALLING)
-		{
-			return _SPI_RETURN.SPI_RETURN_invalid_arg;
+			return SPI_RETURN_T::SPI_RETURN_invalid_arg;
 		}
-		
+
+		if (config.cpha < SPI_CHPA_T::SPI_EDGE_RISING ||
+			config.cpha > SPI_CHPA_T::SPI_EDGE_FALLING)
+		{
+			return SPI_RETURN_T::SPI_RETURN_invalid_arg;
+		}
+
 		#define MIN_BITS_PER_FRAME	4
 		#define MAX_BITS_PER_FRAME	16
-		
+
 		if (config.bits_per_frame < MIN_BITS_PER_FRAME ||
 			config.bits_per_frame > MAX_BITS_PER_FRAME)
 		{
-			return _SPI_RETURN.SPI_RETURN_invalid_arg;
+			return SPI_RETURN_T::SPI_RETURN_invalid_arg;
 		}
-		
-		#define MAX_BAUDRATE	895522389	// tBUS = (1 / fBUS) = 17 ns 
+
+		#define MAX_BAUDRATE	895522389	// tBUS = (1 / fBUS) = 17 ns
 											// DS1 = 4 * tBUS = 67 ns (minimum clock cycle time)
 											// BaudRateMAX = 60 / DS1 = 895,522,388 clock pulses per minute
-		
+
 		if (config.baudrate < MAX_BAUDRATE)
 		{
-			return _SPI_RETURN.SPI_RETURN_invalid_arg;
+			return SPI_RETURN_T::SPI_RETURN_invalid_arg;
 		}
-		
+
 		#define MIN_CS_TO_SCK	350 /* Time (ns) between asserting CS & SCK edge */
-		
+
 		if (config.cs_to_sck > MIN_CS_TO_SCK)
 		{
-			return _SPI_RETURN.SPI_RETURN_invalid_arg;
+			return SPI_RETURN_T::SPI_RETURN_invalid_arg;
 		}
-	
+
 		#define MIN_SCK_TO_CS	350 /* Time (ns) between SCK edge & releasing CS */
-	
+
 		if (config.sck_to_cs > MIN_SCK_TO_CS)
 		{
-			return _SPI_RETURN.SPI_RETURN_invalid_arg;
+			return SPI_RETURN_T::SPI_RETURN_invalid_arg;
 		}
-		
-		return  _SPI_RETURN.SPI_RETURN_ok;
 	}
+	return  SPI_RETURN_T::SPI_RETURN_ok;
 }
 
 
@@ -70,98 +77,21 @@ SPI_RETURN_T SPI_transferBlocking(const SPI_TRANSFER_T *transfer)
 {
 	if (transfer == NULL)
 	{
-		return _SPI_RETURN.SPI_RETURN_nullptr;
+		return SPI_RETURN_T::SPI_RETURN_nullptr;
 	}
 	else if (transfer.number_of_bytes == 0)
 	{
-		return _SPI_RETURN.SPI_RETURN_invalid_arg;
+		return SPI_RETURN_T::SPI_RETURN_invalid_arg;
 	}
 	else
 	{
 		#define SPI_ERROR	-1
-		
+
 		if (SPI_Send_Receive_Bytes(transfer->tx_data, transfer->rx_data, transfer.number_of_bytes) // MCU dependent SPI function to send and receive bytes
 		{
-			return SPI_ERROR
-		}
-		else
-		{
-			return  _SPI_RETURN.SPI_RETURN_ok;
+			return SPI_ERROR;
 		}
 	}
-}
 
-
-/* AS5047D Register Addresses */
-#define ERRFL_ADDR		0x0001
-#define ANGLECOM_ADDR	0x3FFF
-#define SETTINGS1_ADDR	0x0018
-#define SETTINGS2_ADDR	0x0019
-
-
-/* AS5047D configuration structure */
-typedef struct _AS5047D_CONFIG {
-	uint8_t SETTINGS1;
-	uint8_t SETTINGS2;
-} AS5047D_CONFIG_T;
-
-
-/* AS5047D configuration function */
-SPI_RETURN_T AS5047D_configure(const AS5047D_CONFIG_T *config, const SPI_TRANSFER_T *transfer)
-{
-	if (config == NULL ||
-		transfer == NULL)
-	{
-		return _SPI_RETURN.SPI_RETURN_nullptr;
-	}
-	else if (!(config.SETTINGS1 & 1) ||
-			  (config.SETTINGS1 & 2))
-	{
-		return _SPI_RETURN.SPI_RETURN_invalid_arg;		
-	}
-	else
-	{
-		uint8_t TX_DATA[] = {SETTINGS1_ADDR, config.SETTINGS1, SETTINGS2_ADDR, config.SETTINGS2};
-		
-		transfer->tx_data = TX_DATA;
-		transfer->number_of_bytes = 4;
-		
-		SPI_transferBlocking(transfer);
-		
-		return  _SPI_RETURN.SPI_RETURN_ok;
-	}
-}
-
-
-// example of AS5047D_register_read usage when reading an angle in degrees
-void a_function(void)
-{
-	//...
-	
-	#define NO_OF_BYTES	3
-	uint8_t				SPI_return;
-	uint8_t 			RX_DATA[NO_OF_BYTES];
-	uint8_t 			TX_DATA[] = {ERRFL_ADDR, ANGLECOM_ADDR};
-	uint8_t				error;
-	uint16_t			angle = 0, angleRaw = 0;
-
-	
-	SPI_TRANSFER_T 		transfer;		
-	transfer->rx_data = RX_DATA;
-	transfer->tx_data = TX_DATA;
-	transfer->number_of_bytes = NO_OF_BYTES;
-	
-	//...
-	
-	SPI_return = SPI_transferBlocking(transfer);
-	error =  RX_DATA[0];
-	angleRaw = RX_DATA[1];
-	angleRaw += RX_DATA[2] << 8;
-	angle = angleRaw / (double)45.0; // (16,383 / 360)
-	
-	//...
-	
-	return SPI_return;
-	
-	//...
+	return  SPI_RETURN_T::SPI_RETURN_ok;
 }
