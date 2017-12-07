4bcbc960011f67bcd53c6eb8b141aaab960b3415
kostarasae <32982168+kostarasae@users.noreply.github.com>
2017-12-07 14:07:23 +0000

    Update automata_spi.c

--- automata_spi.c
+++ automata_spi.c
@@ -9,65 +9,62 @@
 #include "automata_spi.h"
 
 
-/* Configure the SPI transfer */
-SPI_RETURN_T SPI_configureTransfer(const SPI_CONFIG_T *config)
+SPI_RETURN_T SPI_checkNullPointer(void* pointer)
 {
-	if (config == NULL)
+	if (pointer == NULL)
 	{
 		return SPI_RETURN_T::SPI_RETURN_nullptr;
 	}
-	else
+}
+
+
+SPI_RETURN_T SPI_checkRange(uint32_t min, uint32_t max, uint32_t var)
+{
+	if (var < min)
 	{
-		if (config.cs_pol < SPI_POLARITY_T::SPI_ACTIVE_LOW ||
-			config.cs_pol > SPI_POLARITY_T::SPI_ACTIVE_HIGH)
-		{
-			return SPI_RETURN_T::SPI_RETURN_invalid_arg;
-		}
-
-		if (config.cpol < SPI_CPOL_T::SPI_IDLE_LOW ||
-			config.cpol > SPI_CPOL_T::SPI_IDLE_HIGH)
-		{
-			return SPI_RETURN_T::SPI_RETURN_invalid_arg;
-		}
-
-		if (config.cpha < SPI_CHPA_T::SPI_EDGE_RISING ||
-			config.cpha > SPI_CHPA_T::SPI_EDGE_FALLING)
-		{
-			return SPI_RETURN_T::SPI_RETURN_invalid_arg;
-		}
-
-		#define MIN_BITS_PER_FRAME	4
-		#define MAX_BITS_PER_FRAME	16
-
-		if (config.bits_per_frame < MIN_BITS_PER_FRAME ||
-			config.bits_per_frame > MAX_BITS_PER_FRAME)
-		{
-			return SPI_RETURN_T::SPI_RETURN_invalid_arg;
-		}
-
-		#define MAX_BAUDRATE	895522389	// tBUS = (1 / fBUS) = 17 ns
-											// DS1 = 4 * tBUS = 67 ns (minimum clock cycle time)
-											// BaudRateMAX = 60 / DS1 = 895,522,388 clock pulses per minute
-
-		if (config.baudrate < MAX_BAUDRATE)
-		{
-			return SPI_RETURN_T::SPI_RETURN_invalid_arg;
-		}
-
-		#define MIN_CS_TO_SCK	350 /* Time (ns) between asserting CS & SCK edge */
-
-		if (config.cs_to_sck > MIN_CS_TO_SCK)
-		{
-			return SPI_RETURN_T::SPI_RETURN_invalid_arg;
-		}
-
-		#define MIN_SCK_TO_CS	350 /* Time (ns) between SCK edge & releasing CS */
-
-		if (config.sck_to_cs > MIN_SCK_TO_CS)
-		{
-			return SPI_RETURN_T::SPI_RETURN_invalid_arg;
-		}
+		return SPI_RETURN_T::SPI_RETURN_invalid_arg;
 	}
+
+	if (var > max)
+	{
+		return SPI_RETURN_T::SPI_RETURN_invalid_arg;
+	}
+}
+
+
+/* Configure the SPI transfer */
+SPI_RETURN_T SPI_configureTransfer(const SPI_CONFIG_T *config)
+{
+	SPI_checkNullPointer(config);
+
+	SPI_checkRange(SPI_POLARITY_T::SPI_ACTIVE_LOW, SPI_POLARITY_T::SPI_ACTIVE_HIGH, config.cs_pol);
+
+	SPI_checkRange(SPI_CPOL_T::SPI_IDLE_LOW, SPI_CPOL_T::SPI_IDLE_HIGH, config.cpol);
+
+	SPI_checkRange(SPI_CHPA_T::SPI_EDGE_RISING, SPI_CHPA_T::SPI_EDGE_FALLING, config.cpha);
+
+
+	#define MIN_BITS_PER_FRAME	4
+	#define MAX_BITS_PER_FRAME	16
+	SPI_checkRange(MIN_BITS_PER_FRAME, MAX_BITS_PER_FRAME, config.bits_per_frame);
+
+
+	#define MIN_BAUDRATE	1
+	#define MAX_BAUDRATE	895522389	// tBUS = (1 / fBUS) = 17 ns
+										// DS1 = 4 * tBUS = 67 ns (minimum clock cycle time)
+										// BaudRateMAX = 60 / DS1 = 895,522,388 clock pulses per minute
+	SPI_checkRange(MIN_BAUDRATE, MAX_BAUDRATE, config.baudrate);
+
+
+	#define MIN_CS_TO_SCK	350 /* Time (ns) between asserting CS & SCK edge */
+	#define MAX_CS_TO_SCK	sizeof(uint32_t)
+	SPI_checkRange(MIN_CS_TO_SCK, MAX_CS_TO_SCK, config.cs_to_sck);
+
+
+	#define MIN_SCK_TO_CS	350 /* Time (ns) between SCK edge & releasing CS */
+	#define MAX_SCK_TO_CS	sizeof(uint32_t)
+	SPI_checkRange(MIN_SCK_TO_CS, MAX_SCK_TO_CS, config.sck_to_cs);
+
 	return  SPI_RETURN_T::SPI_RETURN_ok;
 }
 
@@ -75,22 +72,17 @@ SPI_RETURN_T SPI_configureTransfer(const SPI_CONFIG_T *config)
 /* SPI transfer */
 SPI_RETURN_T SPI_transferBlocking(const SPI_TRANSFER_T *transfer)
 {
-	if (transfer == NULL)
-	{
-		return SPI_RETURN_T::SPI_RETURN_nullptr;
-	}
-	else if (transfer.number_of_bytes == 0)
-	{
-		return SPI_RETURN_T::SPI_RETURN_invalid_arg;
-	}
-	else
-	{
-		#define SPI_ERROR	-1
+	SPI_checkNullPointer(transfer);
 
-		if (SPI_Send_Receive_Bytes(transfer->tx_data, transfer->rx_data, transfer.number_of_bytes) // MCU dependent SPI function to send and receive bytes
-		{
-			return SPI_ERROR;
-		}
+	#define MIN_BYTES	1
+	#define MAX_BYTES	sizeof(uint32_t)
+
+	SPI_checkRange(MIN_BYTES, MAX_BYTES, transfer.number_of_bytes);
+
+	#define SPI_ERROR	-1
+	if (SPI_Send_Receive_Bytes(transfer->tx_data, transfer->rx_data, transfer.number_of_bytes) // MCU dependent SPI function to send and receive bytes
+	{
+		return SPI_ERROR;
 	}
 
 	return  SPI_RETURN_T::SPI_RETURN_ok;
