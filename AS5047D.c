65997deea23e7a86060a363a1aff29ec9a69a774
kostarasae <32982168+kostarasae@users.noreply.github.com>
2017-12-07 14:06:22 +0000

    Update AS5047D.c

--- AS5047D.c
+++ AS5047D.c
@@ -1,26 +1,85 @@
+/*
+ * AS5047D.c
+ *
+ *  Created on: 7 Dec 2017
+ *      Author: Kostas Stamatakis
+ */
+
+
 #include "AS5047D.h"
 
 
-/* AS5047D configuration function */
-SPI_RETURN_T AS5047D_configure(const AS5047D_CONFIG_T *config, const SPI_TRANSFER_T *transfer)
+AS5047D_RETURN_T AS5047D_checkNullPointer(void* pointer)
 {
-	if (config == NULL ||
-		transfer == NULL)
+	if (pointer == NULL)
 	{
-		return SPI_RETURN_T::SPI_RETURN_nullptr;
+		return AS5047D_RETURN_T::AS5047D_RETURN_nullptr;
 	}
-	else if (!(config.SETTINGS1 & 1) ||
-			  (config.SETTINGS1 & 2))
-	{
-		return SPI_RETURN_T::SPI_RETURN_invalid_arg;
-	}
-
-	uint8_t TX_DATA[] = {SETTINGS1_ADDR, config.SETTINGS1, SETTINGS2_ADDR, config.SETTINGS2};
-
-	transfer->tx_data = TX_DATA;
-	transfer->number_of_bytes = 4;
-
-	SPI_transferBlocking(transfer);
-
-	return  SPI_RETURN_T::SPI_RETURN_ok;
 }
+
+
+AS5047D_RETURN_T AS5047D_checkConfiguration(const AS5047D_CONFIG_T *config)
+{
+	if (!(config->SETTINGS1 & 1) ||
+		 (config->SETTINGS1 & 2))
+	{
+		return AS5047D_RETURN_T::AS5047D_RETURN_invalid_arg;
+	}
+}
+
+
+int32_t has_even_parity(uint32_t x)
+{
+	uint8_t shift=1;
+    while (shift < (sizeof(x)*8))
+    {
+            x ^= (x>>shift);
+            shift<<=1;
+    }
+    return !(x & 0x1);
+}
+
+
+
+AS5047D_RETURN_T AS5047D_checkFrame(const uint8_t *data, uint32_t bytes)
+{
+	uint32_t i;
+
+	for (i = 0; i < bytes; i++)
+	{
+		if ((data + i) & 2)
+		{
+			return AS5047D_RETURN_T::AS5047D_RETURN_invalid_arg;
+		}
+
+		if (has_even_parity((data + i) & ~0x8000) != ((data + i) & 0x8000))
+		{
+			return AS5047D_RETURN_T::AS5047D_RETURN_invalid_arg;
+		}
+	}
+}
+
+
+
+/* AS5047D configuration function */
+AS5047D_RETURN_T AS5047D_configure(const AS5047D_CONFIG_T *config, const AS5047D_TRANSFER_T *transfer)
+{
+	AS5047D_checkNullPointer(config);
+	AS5047D_checkNullPointer(transfer);
+
+	AS5047D_checkConfiguration(config);
+
+	uint8_t TX_DATA[] = {config->SETTINGS1_ADDR, config->SETTINGS1, config->SETTINGS2_ADDR, config->SETTINGS2};
+	transfer->tx_data = TX_DATA;
+
+	#define NO_OF_REGISTERS		4
+	#define BYTES_PER_REGISTER	2
+	transfer->number_of_bytes = NO_OF_REGISTERS * BYTES_PER_REGISTER;
+
+	AS5047D_checkFrame(transfer->tx_data, transfer->number_of_bytes);
+	AS5047D_transferBlocking(transfer);
+	AS5047D_checkFrame(transfer->rx_data, transfer->number_of_bytes);
+
+	return  AS5047D_RETURN_T::AS5047D_RETURN_ok;
+}
+
